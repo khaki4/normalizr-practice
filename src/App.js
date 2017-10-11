aa0c1fb0fe06b7312859b9e1b86429c5ab618799
정지원 <4dakto@gmail.com>
2017-10-11 22:46:41 +0900

    Jiwon :: init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import testCase form './testCase'
+
+const App = () => {
+  testCase()
+  return (
+    <div>
+      normalizr test
+    </div>
+  )
+}
+
+export default App;
+
+
